6bfba6ae2ea69eacb7a3c631101d4c917965d7ab
Irene Zhang <irenezy.zhang@gmail.com>
2021-04-06 14:02:03 -0400

    base scraping code and cleaning code

---
+++ cleaning_data/clean_data.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+path_data = (".\spotify_dataset_archive\data.csv")
+cols_data = [acousticness, artists,danceability,duration_ms,energy,explicit,id,instrumentalness,key,liveness,loudness,mode,name,popularity,release_date,speechiness,tempo,valence,year]
