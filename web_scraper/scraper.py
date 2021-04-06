03aa0bb868acc1aede51bc48d75d8b0a45551518
Irene Zhang <irenezy.zhang@gmail.com>
2021-04-06 14:06:43 -0400

    update all of github

---
+++ web_scraper/scraper.py
@@ -0,0 +1,8 @@
+from selenium import *
+
+#connect to URL
+#https://medium.com/swlh/how-to-leverage-spotify-api-genius-lyrics-for-data-science-tasks-in-python-c36cdfb55cf3
+
+url = ""
+driver = webdriver.Chrome()
+driver.get(url)
