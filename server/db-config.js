4bfd6a93c65efffee3dcd08207e3ac281012fca9
Linda Ting <lstting@seas.upenn.edu>
2021-04-21 15:39:59 -0400

    add hw2 template code

---
+++ server/db-config.js
@@ -0,0 +1,9 @@
+// Private. Will not be included in submission
+// Replace <Your Pennkey> and <Your Password>
+module.exports = {
+	host: "localhost",
+	user: "root",
+	password: "tata1230",
+	port: "3306",
+	database: "cis550_hw2"
+}
