6b79892d1f8e3647152da55cfb9d98b79c993ec8
Irene Zhang <irenezy.zhang@gmail.com>
2021-04-24 20:22:19 -0400

    scraper base code

--- cleaning_data/clean_data.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-
-path_data = (".\spotify_dataset_archive\data.csv")
-cols_data = [acousticness, artists,danceability,duration_ms,energy,explicit,id,instrumentalness,key,liveness,loudness,mode,name,popularity,release_date,speechiness,tempo,valence,year]
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,8 +1,64 @@
-from selenium import *
+import spotipy
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials
 
 #connect to URL
 #https://medium.com/swlh/how-to-leverage-spotify-api-genius-lyrics-for-data-science-tasks-in-python-c36cdfb55cf3
 
-url = ""
-driver = webdriver.Chrome()
-driver.get(url)
+cid = "67f61ec9b74f4051bd316ce2c391e199"
+secret = "e3ee535a963140dbbd27feacc58262d5"
+
+client_cred_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_cred_manager)
+
+#insert the URI as a string into the function
+def get_album_tracks(uri_info):
+    uri = []
+    track = []
+    duration = []
+    explicit = []
+    track_number = []
+    one = sp.album_tracks(uri_info, limit=50, offset=0, market='US')
+    df1 = pd.DataFrame(one)
+
+    for i, x in df1['items'].items():
+        uri.append(x['uri'])
+        track.append(x['name'])
+        duration.append(x['duration_ms'])
+        explicit.append(x['explicit'])
+        track_number.append(x['track_number'])
+
+    df2 = pd.DataFrame({
+    'uri':uri,
+    'track':track,
+    'duration_ms':duration,
+    'explicit':explicit,
+    'track_number':track_number})
+
+    return df2
+
+get_album_tracks('https://open.spotify.com/album/64WayBZ9Mrx8iGOpME5vpI?si=64PAID4gRqOPWH6n8tQ9wQ')
+
+#function to scrape lyrics from genius
+def scrape_lyrics(artistname, songname):
+    artistname2 = str(artistname.replace(' ','-')) if ' ' in artistname else str(artistname)
+    songname2 = str(songname.replace(' ','-')) if ' ' in songname else str(songname)
+    page = requests.get('https://genius.com/'+ artistname2 + '-' + songname2 + '-' + 'lyrics')
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics1 = html.find("div", class_="lyrics")
+    lyrics2 = html.find("div", class_="Lyrics__Container-sc-1ynbvzw-2 jgQsqn")
+    if lyrics1:
+        lyrics = lyrics1.get_text()s
+    elif lyrics2:
+        lyrics = lyrics2.get_text()
+    elif lyrics1 == lyrics2 == None:
+        lyrics = None
+    return lyrics
+
+#function to attach lyrics onto data frame
+#artist_name should be inserted as a string
+def lyrics_onto_frame(df1, artist_name):
+    for i,x in enumerate(df1['track']):
+        test = scrape_lyrics(artist_name, x)
+        df1.loc[i, 'lyrics'] = test
+    return df1
