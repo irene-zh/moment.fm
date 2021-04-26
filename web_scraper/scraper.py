6eb6dcd1f19ab363d2b4847182ce57c440bd4331
Irene Zhang <iyzhang@Irenes-MacBook-Pro.local>
2021-04-25 21:06:16 -0400

    updated scraper code

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,12 +1,13 @@
 import spotipy
 import sys
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
 
 #connect to URL
 #https://medium.com/swlh/how-to-leverage-spotify-api-genius-lyrics-for-data-science-tasks-in-python-c36cdfb55cf3
 
 cid = "67f61ec9b74f4051bd316ce2c391e199"
-secret = "e3ee535a963140dbbd27feacc58262d5"
+secret = ""
 
 client_cred_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_cred_manager)
@@ -37,7 +38,7 @@ def get_album_tracks(uri_info):
 
     return df2
 
-get_album_tracks('https://open.spotify.com/album/64WayBZ9Mrx8iGOpME5vpI?si=64PAID4gRqOPWH6n8tQ9wQ')
+bts = get_album_tracks('spotify:album:6mJZTV8lCqnwftYZa94bXS')
 
 #function to scrape lyrics from genius
 def scrape_lyrics(artistname, songname):
@@ -48,7 +49,7 @@ def scrape_lyrics(artistname, songname):
     lyrics1 = html.find("div", class_="lyrics")
     lyrics2 = html.find("div", class_="Lyrics__Container-sc-1ynbvzw-2 jgQsqn")
     if lyrics1:
-        lyrics = lyrics1.get_text()s
+        lyrics = lyrics1.get_text()
     elif lyrics2:
         lyrics = lyrics2.get_text()
     elif lyrics1 == lyrics2 == None:
@@ -62,3 +63,5 @@ def lyrics_onto_frame(df1, artist_name):
         test = scrape_lyrics(artist_name, x)
         df1.loc[i, 'lyrics'] = test
     return df1
+
+print(lyrics_onto_frame(bts, 'BTS'))
