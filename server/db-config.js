c6a45ad466f8b2c72661d37daf5840324c57127a
Linda Ting <lstting@seas.upenn.edu>
2021-05-04 14:17:51 -0400

    cleanup server index

--- server/db-config.js
+++ server/db-config.js
@@ -3,7 +3,7 @@
 module.exports = {
 	host: "cis550proj.c6uc5gtiukkv.us-east-2.rds.amazonaws.com",
 	user: "linda",
-	password: "Fourstink!!",
+	password: "Fourstinky!!",
 	port: "1521",
 	database: "MOMENTDB"
 }
--- server/index.js
+++ server/index.js
@@ -19,29 +19,6 @@ app.get('/explore/songs/:title', routes.getRecommendedSongs);
 
 app.get('/explore/artists/:name', routes.getRecommendedArtists);
 
-/* ---- (Dashboard) ---- */
-// The route localhost:8081/keywords is registered to the function
-// routes.getTop20Keywords, specified in routes.js.
-app.get('/keywords', routes.getTop20Keywords);
-
-
-/* ---- Q1b (Dashboard) ---- */
-app.get('/keywords/:keyword', routes.getTopMoviesWithKeyword);
-
-
-/* ---- Q2 (Recommendations) ---- */
-app.get('/recommendations/:movie', routes.getRecs);
-
-
-/* ---- (Best Movies) ---- */
-app.get('/decades', routes.getDecades);
-app.get('/genres', routes.getGenres);
-
-
-/* ---- Q3b (Best Movies) ---- */
-app.get('/bestmovies/:decade/:genre', routes.bestMoviesPerDecadeGenre)
-
-
 app.listen(8081, () => {
 	console.log('Server listening on PORT 8081');
 });
