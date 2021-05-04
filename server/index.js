b5796c9b4551298feaff0e6f4a5632e6bfad4853
Linda Ting <lstting@seas.upenn.edu>
2021-05-04 17:04:08 -0400

    set up homepage stats rendering, routes, and queries

--- server/index.js
+++ server/index.js
@@ -19,9 +19,23 @@ app.get('/song/:id', routes.getSong);
 
 app.get('/explore/songs/:title', routes.getRecommendedSongs);
 
-app.get('/explore/artists/:name', routes.getRecommendedArtists);
+app.get('/explore/artists/:artist_name', routes.getRecommendedArtists);
 
 app.get('/iGotAFeeling/', routes.getIGotAFeeling);
+
+app.get('/saddestOf2020', routes.getSaddest2020);
+
+app.get('/hiphop2018', routes.getHipHop2018);
+
+app.get('/metal2005', routes.getMetal2005);
+
+app.get('/popular2020', routes.getSongsPopular2020);
+
+app.get('/frequency2019', routes.getArtistsFrequent2019);
+
+app.get('/activePop', routes.getArtistsActivePop);
+
+app.get('/artistRelevance', routes.getArtistsRelevance);
 
 app.listen(8081, () => {
 	console.log('Server listening on PORT 8081');
