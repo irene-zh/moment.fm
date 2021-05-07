887feca5850cce34f64c6a49e49789f92755b2ef
Felix <felixcantdebate@gmail.com>
2021-05-07 17:31:44 -0400

    trying to implement get saddest songs

--- server/routes.js
+++ server/routes.js
@@ -14,6 +14,22 @@ const getExample = (req, res) => {
   });
 };
 
+//saddest songs (lowest energy) of 2020
+const getSaddest2020 = (req, res) => {
+  const query = `
+    SELECT s.track_id AS id, s.name AS name, s.artist AS artist
+	FROM Songs s
+	WHERE s.musical_mode = 0 AND s.duration_ms >= 120000 AND EXTRACT(YEAR FROM s.release_date) = 2020
+	ORDER BY s.danceability ASC, s.energy ASC, s.loudness ASC
+	FETCH NEXT 10 ROWS ONLY
+  `;
+
+  connection.query(query, (rows) => {
+    res.json(rows);
+  });
+};
+
 module.exports = {
   getExample: getExample,
+  getSaddest2020: getSaddest2020,
 };
